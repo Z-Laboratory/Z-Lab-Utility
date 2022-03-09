2c66e40533402478327b08eeccb7eb81a404e45c
k50112113 <k50112113@gmail.com>
2022-03-09 14:08:29 -0600

    Update example.py

--- QENS data fitting/example.py
+++ QENS data fitting/example.py
@@ -4,7 +4,6 @@ import QENSFit as QF
 
 # example code for fitting resolution function
 grpfilename = "<name of your resolution data file>.grp"
-grpfilename = "Resolution_file_265C_3.32meV_FLiNaK.grp"
 res = QF.ResolutionDataModel(grpfilename = grpfilename, data_range = 0.4, neutron_e0 = 3.32)
 res.fit()
 res.output_results() #default output file: fitting_results_ < name of your resolution data file > .txt
@@ -13,9 +12,7 @@ res.plot_results(log_scale = True)
 
 # example code for fitting QENS Data
 grpfilename = "<name of your QENS data file>.grp"
-grpfilename = "FLiNaK_1073K_3.32meV.grp"
 resolution_parameter_filename = "fitting_results_<name of your resolution data file>.txt"
-resolution_parameter_filename = "fitting_results_Resolution_file_265C_3.32meV_FLiNaK.txt"
 qens = QF.QENSDataModel(grpfilename = grpfilename, resolution_parameter_filename = resolution_parameter_filename, neutron_e0 = 3.32, data_range = 0.4)
 qens.fit(fix_elastic_contribution = 0.1)
 qens.output_results()
