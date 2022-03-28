48a7fad41f6c6464b6b9636b7c221d64439a736a
k50112113 <k50112113@gmail.com>
2022-03-27 21:56:25 -0500

    Update example.py

--- QENS data fitting/example.py
+++ QENS data fitting/example.py
@@ -4,12 +4,12 @@ import QENSFit as QF
 
 # example code for fitting resolution function
 res_output_dir = "0.5mev-resolution"
-# grpfilename = "Resolution_file_265C_3.32meV_FLiNaK.grp"
-# res = QF.ResolutionDataModel(grpfilename = grpfilename, data_range = 0.5, neutron_e0 = 3.32, max_n_gauss = 4)
-# res.fit()
-# res.output_results(output_dir = res_output_dir) #default output file: fitting_results_Resolution_file_265C_3.32meV_FLiNaK.txt
-# res.plot_results(output_dir = res_output_dir, show_errorbar = False)
-# res.plot_results(output_dir = res_output_dir, log_scale=True, show_errorbar = False)
+grpfilename = "Resolution_file_265C_3.32meV_FLiNaK.grp"
+res = QF.ResolutionDataModel(grpfilename = grpfilename, data_range = 0.5, neutron_e0 = 3.32, max_n_gauss = 4)
+res.fit()
+res.output_results(output_dir = res_output_dir) #default output file: fitting_results_Resolution_file_265C_3.32meV_FLiNaK.txt
+res.plot_results(output_dir = res_output_dir, show_errorbar = False)
+res.plot_results(output_dir = res_output_dir, log_scale=True, show_errorbar = False)
 
 # example code for fitting QENS Data
 qens_output_dir = "773k/0.5mev-test"
